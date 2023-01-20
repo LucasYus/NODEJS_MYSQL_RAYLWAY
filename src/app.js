78ed3c7a032fbfd79a65eb05761da41e3152916c
Lucas <lucasyustasalvadores03@gmail.com>
2023-01-20 19:32:00 +0100

    hardcoded fixed2

--- src/app.js
+++ src/app.js
@@ -4,19 +4,19 @@ import {PORT} from './config.js'
 //la creacion de la aplicacionweb en Nodejs con express
 const app = Express()
 //el puerto de nuestra aplicacion web va a  ser el 3000
-app.listen (3000)
+app.listen (PORT)
 console.log('servidor en el puerto 3000')
 
 app.get ('/', (req,res) => {
-    res.send ("Implantacion aplicaciones web2")
+    res.send ('SELECT * from users')
 })
 
 app.get ('/ping', async(req,res) => {
-   const result = await pool.query ('SELECT * from users')
+   const result = await pool.query (`SELECT "Hola Lucas" AS RESULT`)
    console.log (result[0])
 })
 
 app.get ('/create', async(req,res) => {
-    const insert =  await pool.query (`INSERT INTO users(name) VALUES ("Hector")`)
+    const insert =  await pool.query (`INSERT INTO users(name) VALUES ("Lucas")`)
    console.log (insert)
  })
